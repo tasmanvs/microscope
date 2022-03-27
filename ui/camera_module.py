362d23c96af17480baa7e7c03fe977139203eccf
Tasman Smith <tasmanvs@gmail.com>
2022-03-27 11:34:53 -0700

    Connected signals to lambdas

--- ui/camera_module.py
+++ ui/camera_module.py
@@ -25,3 +25,11 @@ class CameraModule(object):
     def take_picture_clicked(self):
         self.status_ = "Take Picture Clicked"
         self.print_status()
+    
+    def file_name_changed(self, text):
+        self.file_name_ = text
+        self.status_ = "Set file name to " + self.file_name_
+        self.print_status()
+
+    def get_status_text(self) -> str:
+        return self.status_
